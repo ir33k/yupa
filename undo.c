842ec3d01cface0fc925cb619f7e3b1ef37bc1d2
irek <irek@gabr.pl>
2025-06-25 11:14:22 +0200

    Refactor undo

--- undo.c
+++ undo.c
@@ -1,49 +1,28 @@
-#include <stdio.h>
 #include <string.h>
 #include "undo.h"
 
-/* NOTE(irek): This browser undo history is rather small.  But browser
- * will most likely be used for short session and if there will be a
- * need to go back further in history then persistent undo history
- * file can be used instead. */
-
-#define SIZE 64
+#define SIZE 32
 
 static char ring[SIZE][4096]={0};
 static unsigned last=0;
 
 void
-undo_add(char *uri)
+undo_add(char *str)
 {
-	if (!strcmp(ring[last % SIZE], uri))
-		return;
-
-	last++;
-	strcpy(ring[last % SIZE], uri);
-	ring[(last+1) % SIZE][0] = 0;
+	if (strcmp(ring[last % SIZE], str)) {
+		strcpy(ring[++last % SIZE], str);
+		ring[(last+1) % SIZE][0] = 0;
+	}
 }
 
 char *
-undo_go(int offset)
+undo_go(int n)
 {
-	if (last == 0)
-		return 0;
+	int i=n, d = n>0 ? -1 : +1;
 
-	while (offset < 0) {
-		if (ring[(last-1) % SIZE][0] == 0)
-			return 0;
+	for (; last && i; i+=d, last-=d)
+		if (!ring[(last -d) % SIZE][0])
+			break;
 
-		last--;
-		offset++;
-	}
-
-	while (offset > 0) {
-		if (ring[(last+1) % SIZE][0] == 0)
-			return 0;
-
-		last++;
-		offset--;
-	}
-
-	return ring[last % SIZE];
+	return i == n ? 0 : ring[last % SIZE];
 }
