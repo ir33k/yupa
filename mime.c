84ccd243f57dd9cb407f3542a4f37f22c867b16d
irek <irek@gabr.pl>
2025-07-06 20:46:51 +0200

    Fix mime detection by file extension

--- mime.c
+++ mime.c
@@ -14,23 +14,23 @@ mime_path(char *path)
 
 	extension = strrchr(path, '.');
 	if (!extension) return BINARY;
-	if (strcasecmp(extension, ".txt"))  return TEXT;
-	if (strcasecmp(extension, ".md"))   return TEXT;
-	if (strcasecmp(extension, ".gph"))  return GPH;
-	if (strcasecmp(extension, ".gmi"))  return GMI;
-	if (strcasecmp(extension, ".html")) return HTML;
-	if (strcasecmp(extension, ".jpg"))  return IMAGE;
-	if (strcasecmp(extension, ".jpeg")) return IMAGE;
-	if (strcasecmp(extension, ".png"))  return IMAGE;
-	if (strcasecmp(extension, ".bmp"))  return IMAGE;
-	if (strcasecmp(extension, ".gif"))  return IMAGE;
-	if (strcasecmp(extension, ".mp4"))  return VIDEO;
-	if (strcasecmp(extension, ".mov"))  return VIDEO;
-	if (strcasecmp(extension, ".avi"))  return VIDEO;
-	if (strcasecmp(extension, ".mkv"))  return VIDEO;
-	if (strcasecmp(extension, ".wav"))  return AUDIO;
-	if (strcasecmp(extension, ".mp3"))  return AUDIO;
-	if (strcasecmp(extension, ".pdf"))  return PDF;
+	if (!strcasecmp(extension, ".txt"))  return TEXT;
+	if (!strcasecmp(extension, ".md"))   return TEXT;
+	if (!strcasecmp(extension, ".gph"))  return GPH;
+	if (!strcasecmp(extension, ".gmi"))  return GMI;
+	if (!strcasecmp(extension, ".html")) return HTML;
+	if (!strcasecmp(extension, ".jpg"))  return IMAGE;
+	if (!strcasecmp(extension, ".jpeg")) return IMAGE;
+	if (!strcasecmp(extension, ".png"))  return IMAGE;
+	if (!strcasecmp(extension, ".bmp"))  return IMAGE;
+	if (!strcasecmp(extension, ".gif"))  return IMAGE;
+	if (!strcasecmp(extension, ".mp4"))  return VIDEO;
+	if (!strcasecmp(extension, ".mov"))  return VIDEO;
+	if (!strcasecmp(extension, ".avi"))  return VIDEO;
+	if (!strcasecmp(extension, ".mkv"))  return VIDEO;
+	if (!strcasecmp(extension, ".wav"))  return AUDIO;
+	if (!strcasecmp(extension, ".mp3"))  return AUDIO;
+	if (!strcasecmp(extension, ".pdf"))  return PDF;
 
 	return 0;
 }
